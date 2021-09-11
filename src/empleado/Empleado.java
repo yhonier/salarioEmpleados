ba36269b839ae902b6446f7e0348839a076c7a49
Yhonier <yhoniermolina@gmail.com>
2021-09-10 19:07:48 -0500

    Se crea la clase empleado

---
+++ src/empleado/Empleado.java
@@ -0,0 +1,81 @@
+
+package empleado;
+
+public class Empleado {
+    
+    //Atributos=Vairables=Datos
+    
+   private String id;
+   private   String nombre;
+   private   String cargo;
+   private double horasemananes;
+   private int  salario;
+   
+    
+    //Metodo=Accion=Fucion especial
+      
+  
+      
+      
+      //Contructor
+
+    public Empleado() {
+    }
+    
+      //Metodo=Accion=Fucion especial
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getHorasemananes() {
+        return horasemananes;
+    }
+
+    public void setHorasemananes(double horasemananes) {
+        this.horasemananes = horasemananes;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public int caluclarsalario (double horas, int valor){
+    
+    return 0;
+    }
+    
+}
--- src/evilempleados/EvilEmpleados.java
+++ src/evilempleados/EvilEmpleados.java
@@ -1,21 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+La empresa Evil Corp desea reguistar 10 empleados 
  */
 package evilempleados;
 
-/**
- *
- * @author 505
- */
+
 public class EvilEmpleados {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+  
+        
+     
     }
+    
+          public void jugar(){
+        
+        }
+          
     
 }
